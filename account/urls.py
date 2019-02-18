20d02e9421cebc5e9c6e33b299b7c82e9d3c816d
yuyao <yxst1314@gmail.com>
2019-02-18 22:37:37 +0800

    fix login bug,细节处理，增加错误页面

--- account/urls.py
+++ account/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path,include
-from .views import user_reg,user_info
+from .views import user_reg,user_info,user_login
 
 urlpatterns = [
     path('register/',user_reg,name = 'register'),
+    path('login/',user_login,name = 'login'),
     path('user_info/',user_info,name = 'user_info'),
     #使用django自带用户账户方法
     path('',include('django.contrib.auth.urls')),
