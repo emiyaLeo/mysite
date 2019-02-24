18f606e57dc819e5beded147d0f2a34cd9aa3a23
yuyao <yxst1314@gmail.com>
2019-02-24 23:18:55 +0800

    改写登出代码

--- account/urls.py
+++ account/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path,include
-from .views import user_reg,user_info,user_login
+from .views import user_reg,user_info,user_login,user_logout
 
 urlpatterns = [
     path('register/',user_reg,name = 'register'),
     path('login/',user_login,name = 'login'),
+    path('logout/',user_logout,name = 'logout'),
     path('user_info/',user_info,name = 'user_info'),
     #使用django自带用户账户方法
-    path('',include('django.contrib.auth.urls')),
+    # path('',include('django.contrib.auth.urls')),
 ]
