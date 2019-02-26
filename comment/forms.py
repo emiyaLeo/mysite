98d3879e611e3e0eb82e297d2accd2d7451b9673
yuyao <yxst1314@gmail.com>
2019-02-26 23:01:46 +0800

    去除富文本评论功能

--- comment/forms.py
+++ comment/forms.py
@@ -1,10 +1,8 @@
 from django import forms
 from .models import Comment
-from ckeditor.widgets import CKEditorWidget
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('text',)
         labels = {'text':''}
-        widgets = {'text':CKEditorWidget}
